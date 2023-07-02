d50f6a4c590d292bc6245368bae8e9a05dc031c1
islam <islamkazproject@gmail.com>
2023-07-02 22:21:08 +0300

    add models beta

--- mailing_service/mailings/models.py
+++ mailing_service/mailings/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Client(models.Model):
+    phone_number = models.CharField(max_length=11)
+    mobile_operator_code = models.CharField(max_length=10)
+    tag = models.CharField(max_length=255)
+    timezone = models.CharField(max_length=255)
+
+
+class Mailing(models.Model):
+    start_datetime = models.DateTimeField(default=timezone.now)
+    message_text = models.TextField()
+    client_filter = models.Q()
+    end_datetime = models.DateTimeField()
+
+    def is_active(self):
+        return self.start_datetime <= timezone.now() <= self.end_datetime
+
+
+class Message(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=255)
+    mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+
+
